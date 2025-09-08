eae1e725d680df5af9a204aac80f482ef0577fd4
Sareena <sareenamann@berkeley.edu>
2025-09-08 09:59:00 -0700

    generic solver implemented

--- src/solver.py
+++ src/solver.py
@@ -7,8 +7,8 @@ class SolutionRecord:
     """
     Data regarding a specific position in a sequential game.
     """
-
     value: Value
+    sol: dict[int, value]
 
 
 class Solver:
@@ -20,6 +20,24 @@ class Solver:
         returns a mapping of their encodings to their game-theoretic values in
         a dictionary (and other information we may care about).
         """
+        #Ex: make run ARGS="zero_by 10 1 2"
+        current = game.start(game)
+        Solver.solve_helper(current, game)
 
-        # TODO: Homefun 1, Homefun 2
-        ...
+        #Think about memoizing later
+
+    @staticmethod
+    def solve_helper(current, game):
+        # This is a recursive function that explores all game states
+        next = game.get_moves(current)
+
+        #Base Case
+        if len(next) == 0:
+            return
+
+        for n in next:
+            print(n + ": " + game.terminal_value(current))
+            Solver.solve_helper(game.do_move(current, n))
+
+
+    #Later implement memoize so you don[t have to re-compute
