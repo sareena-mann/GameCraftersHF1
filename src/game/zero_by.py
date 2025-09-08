35c458f469543b31c50a4c07dcdb83b218974b8b
Sareena <sareenamann@berkeley.edu>
2025-09-08 10:08:33 -0700

    type casting str and int needed for making moves

--- src/game/zero_by.py
+++ src/game/zero_by.py
@@ -5,17 +5,17 @@ class ZeroBy(Game):
     def __init__(self, args: list[str]):
         # Example: ARGS="zero_by 10 1 2"
         self.args = args
-        self.steps = args[2:]
+        self.steps = args[1:]
 
     def start(self) -> int:
-        return int(list[1])
+        return int(self.args[0])
 
     def get_moves(self, position: int) -> list[int]:
         # Explore all possible places to go one step down from current position
         moves: list[int] = []
         for i in self.steps:
-            if i <= position:
-                moves.append(position - i)
+            if int(i) <= position:
+                moves.append(position - int(i))
         return moves
 
     def do_move(self, position: int, move: int) -> int:
