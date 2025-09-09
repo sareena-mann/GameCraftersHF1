e528bab5f0dee19b4d0319147ab8395ee5b08560
Sareena <sareenamann@berkeley.edu>
2025-09-08 23:08:45 -0700

    debugging, still something wrong with terminal values

--- src/game/zero_by.py
+++ src/game/zero_by.py
@@ -14,19 +14,21 @@ class ZeroBy(Game):
         # Explore all possible places to go one step down from current position
         moves: list[int] = []
         for i in self.steps:
-            if int(i) <= position:
+            if int(i) < position:
                 moves.append(position - int(i))
         return moves
 
     def do_move(self, position: int, move: int) -> int:
-        if move <= position:
+        if position >= move:
             return position - move
         return 0
 
     def terminal_value(self, position: int) -> Value:
         # get_moves and check: All W children --> LOSE  // at least 1 L children --> WIN
-        outcomes = self.get_moves(self, position)
+        outcomes = self.get_moves(position)
         for pos in outcomes:
-            if pos == Value.LOSE:
+            if pos == 0:
+                return Value.WIN
+            elif self.terminal_value(pos) == Value.LOSE:
                 return Value.WIN
         return Value.LOSE
